7f5ce23d389f346a7f79b20d24c8d8dbfaafc474
Gonzalo Herrera Llobeta <gonzalohll@gmail.com>
2018-08-08 17:19:26 -0300

    Primera interaccion fron/backend

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div >
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="e-mail" v-model="email">
+    <input type="password" name="pass" placeholder="password" v-model="pass">
+    <button @click="register">Registrar</button>
+
+</div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  data () {
+    return {
+        email:'',
+        pass:''
+    }
+  },
+  watch:{
+    email(value){
+        //console.log(value)
+    }
+  },
+  methods: {
+    async register(){
+        const response = await AuthenticationService.register({
+            email: this.email,
+            password: this.pass
+        })
+        console.log(response.data);
+        
+    }
+  }
+}
+</script>
